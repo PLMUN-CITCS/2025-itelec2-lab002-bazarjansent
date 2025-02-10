64061f3754ee617b83720c36003a3fd88143048f
bazarjansent <178275932+bazarjansent@users.noreply.github.com>
2025-02-10 20:29:15 +0800

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -1,13 +1,14 @@
-# JANSENT BAZAR
+# BAZAR JANSENT
 # ITELEC2
-# Problem Set 01 - Problem 01
-# Simple Calculator Program
+# Problem Set 01 - Problem 02
+# Square the Number Program
+
 def main():
     print("Square the Number Program")
-    
+
     number = int(input("Enter a number: "))
-    
-    print(f"The square of {number} is {(number*number):.2f}")
-    
+
+    print(f"The square of {number} is {(number * number):.2f}")
+
 if _name_ == "_main_":
     main()
