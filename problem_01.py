f8a3140678b32b6cb1b59c471b0749a3e7dc629e
bazarjansent <178275932+bazarjansent@users.noreply.github.com>
2025-02-10 18:12:41 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,20 +1,15 @@
-# JANSENT BAZAR
+# YOUR NAME
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    num1 = int(input("Enter 1st number: "))
-    num2 = int(input("Enter 2nd number: "))
-
-   print(f"The sum is {num1 + num2}")
-   print(f"The difference is {num1 - num2}")
-   print(f"The product is {num1 * num2}")
-if num2 != 0:
-    print(f"The quotient is {num1 / num2:.2f}")
-else:
-    print("Cannot divide by zero.")
-
-
+print("Simple Calculator Program")
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print(f"The sum is {num1 + num2}")
+print(f"The difference is {num1 - num2}")
+print(f"The product is {num1 * num2}")
+print(f"The quotient is {(num1 / num2):.2f}")
 if __name__ == "__main__":
     main()
