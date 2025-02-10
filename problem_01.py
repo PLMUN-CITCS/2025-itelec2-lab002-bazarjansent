88caaef14fc5b508ef99c64e7ca6f8c050ece689
bazarjansent <178275932+bazarjansent@users.noreply.github.com>
2025-02-10 16:08:00 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,4 +1,4 @@
-# YOUR NAME
+# JANSENT BAZAR
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
@@ -6,9 +6,10 @@
 def main():
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
-print(f"The sum is {num1 + num2}")
-print(f"The difference is {num1 - num2}")
-print(f"The product is {num1 * num2}")
+
+   print(f"The sum is {num1 + num2}")
+   print(f"The difference is {num1 - num2}")
+   print(f"The product is {num1 * num2}")
 if num2 != 0:
     print(f"The quotient is {num1 / num2:.2f}")
 else:
