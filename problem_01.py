067e0cb24d39907e53e1c12accbcc90be1e572b1
bazarjansent <178275932+bazarjansent@users.noreply.github.com>
2025-02-10 19:07:23 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -16,7 +16,7 @@ def main():
     print(f"The difference is {num1 - num2}")
     
     print(f"The product is {num1 * num2}")
-       
+    
     print(f"The quotient is {(num1 / num2):.2f}")
     
 if __name__ == "__main__":
